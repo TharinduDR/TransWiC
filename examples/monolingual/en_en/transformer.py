624e25c7d22d4108ade70abe54a9652da3f1e6ea
TharinduDR <rhtdranasinghe@gmail.com>
2020-11-11 19:25:38 +0000

    004: Adding transformer model

--- examples/monolingual/en_en/transformer.py
+++ examples/monolingual/en_en/transformer.py
@@ -7,6 +7,8 @@ import torch
 import numpy as np
 
 from sklearn.model_selection import train_test_split
+
+from examples.common.evaluation import weighted_f1, macro_f1
 from examples.common.reader import read_training_file
 from examples.monolingual.en_en.transformer_config import DATA_DIRECTORY, TEMP_DIRECTORY, \
     transformer_config, MODEL_NAME, MODEL_TYPE
@@ -18,6 +20,7 @@ if not os.path.exists(TEMP_DIRECTORY):
 
 train = read_training_file(os.path.join(DATA_DIRECTORY, "training.en-en.data"), os.path.join(DATA_DIRECTORY, "training.en-en.gold"))
 dev = read_training_file(os.path.join(DATA_DIRECTORY, "trial.en-en.data"), os.path.join(DATA_DIRECTORY, "trial.en-en.gold"))
+
 
 if transformer_config["evaluate_during_training"]:
     if transformer_config["n_fold"] > 1:
@@ -31,12 +34,12 @@ if transformer_config["evaluate_during_training"]:
                                args=transformer_config)
             train_df, eval_df = train_test_split(train, test_size=0.1, random_state=transformer_config['manual_seed'] * i)
 
-            model.train_model(train_df, eval_df=eval_df, acc=sklearn.metrics.accuracy_score)
+            model.train_model(train_df, eval_df=eval_df, macro_f1=macro_f1, weighted_f1=weighted_f1, accuracy=sklearn.metrics.accuracy_score)
 
             model = MonoTransWiCModel(MODEL_TYPE, transformer_config["best_model_dir"], num_labels=2,
                                use_cuda=torch.cuda.is_available(), args=transformer_config)
 
-            result, model_outputs, wrong_predictions = model.eval_model(dev, acc=sklearn.metrics.accuracy_score)
+            result, model_outputs, wrong_predictions = model.eval_model(dev, macro_f1=macro_f1, weighted_f1=weighted_f1, accuracy=sklearn.metrics.accuracy_score)
 
             dev_preds[:, i] = model_outputs
 
@@ -48,18 +51,17 @@ if transformer_config["evaluate_during_training"]:
         train_df, eval_df = train_test_split(train, test_size=0.1, random_state=transformer_config['manual_seed'])
 
         start = time.time()
-        model.train_model(train_df, eval_df=eval_df, acc=sklearn.metrics.accuracy_score)
+        model.train_model(train_df, eval_df=eval_df, macro_f1=macro_f1, weighted_f1=weighted_f1, accuracy=sklearn.metrics.accuracy_score)
         end = time.time()
-
 
         model = MonoTransWiCModel(MODEL_TYPE, transformer_config["best_model_dir"], num_labels=2,
                            use_cuda=torch.cuda.is_available(), args=transformer_config)
-        result, model_outputs, wrong_predictions = model.eval_model(dev, acc=sklearn.metrics.accuracy_score)
+        result, model_outputs, wrong_predictions = model.eval_model(dev, macro_f1=macro_f1, weighted_f1=weighted_f1, accuracy=sklearn.metrics.accuracy_score)
         dev['predictions'] = model_outputs
 
 else:
     model = MonoTransWiCModel(MODEL_TYPE, MODEL_NAME, num_labels=2, use_cuda=torch.cuda.is_available(),
                        args=transformer_config)
-    model.train_model(train, acc=sklearn.metrics.accuracy_score)
-    result, model_outputs, wrong_predictions = model.eval_model(dev, acc=sklearn.metrics.accuracy_score)
+    model.train_model(train, macro_f1=macro_f1, weighted_f1=weighted_f1, accuracy=sklearn.metrics.accuracy_score)
+    result, model_outputs, wrong_predictions = model.eval_model(dev, macro_f1=macro_f1, weighted_f1=weighted_f1, accuracy=sklearn.metrics.accuracy_score)
     dev['predictions'] = model_outputs
