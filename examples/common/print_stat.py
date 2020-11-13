1e6828a088ef36e7318fb7cf266df322cf5a745e
TharinduDR <rhtdranasinghe@gmail.com>
2020-11-13 12:32:41 +0000

    004: Adding transformer model

--- examples/common/print_stat.py
+++ examples/common/print_stat.py
@@ -1,25 +1,17 @@
-from sklearn.metrics import recall_score, precision_score, f1_score
+from sklearn.metrics import recall_score, precision_score, f1_score, accuracy_score
 
 
 def print_information(df, pred_column, real_column, pos_column):
     predictions = df[pred_column].tolist()
     real_values = df[real_column].tolist()
 
-    labels = set(real_values)
-
     print()
+    print("Accuracy {}".format(accuracy_score(real_values, predictions)))
     print("Weighted Recall {}".format(recall_score(real_values, predictions, average='weighted')))
     print("Weighted Precision {}".format(precision_score(real_values, predictions, average='weighted')))
-    print("Weighter F1 Score {}".format(f1_score(real_values, predictions, average='weighted')))
+    print("Weighted F1 Score {}".format(f1_score(real_values, predictions, average='weighted')))
 
     print("Macro F1 Score {}".format(f1_score(real_values, predictions, average='macro')))
-
-    for label in labels:
-        print()
-        print("Stat of the {} Class".format(label))
-        print("Recall {}".format(recall_score(real_values, predictions, labels=labels, pos_label=label)))
-        print("Precision {}".format(precision_score(real_values, predictions, labels=labels, pos_label=label)))
-        print("F1 Score {}".format(f1_score(real_values, predictions, labels=labels, pos_label=label)))
 
     pos_values = sorted(list(set(df[pos_column].tolist())))
 
@@ -29,16 +21,10 @@ def print_information(df, pred_column, real_column, pos_column):
         temp_real_values = temp_df[real_column].tolist()
         print ()
         print("Stat of the {} value".format(pos_value))
+        print("Accuracy {}".format(accuracy_score(temp_real_values, temp_predictions)))
         print("Weighted Recall {}".format(recall_score(temp_real_values, temp_predictions, average='weighted')))
         print("Weighted Precision {}".format(precision_score(temp_real_values, temp_predictions, average='weighted')))
-        print("Weighter F1 Score {}".format(f1_score(temp_real_values, temp_predictions, average='weighted')))
-
-        for label in labels:
-            print()
-            print("Stat of the {} Class".format(label))
-            print("Recall {}".format(recall_score(temp_real_values, temp_predictions, labels=labels, pos_label=label)))
-            print("Precision {}".format(precision_score(temp_real_values, temp_predictions, labels=labels, pos_label=label)))
-            print("F1 Score {}".format(f1_score(temp_real_values, temp_predictions, labels=labels, pos_label=label)))
-
+        print("Weighted F1 Score {}".format(f1_score(temp_real_values, temp_predictions, average='weighted')))
+        print("Macro F1 Score {}".format(f1_score(temp_real_values, temp_predictions, average='macro')))
 
 
