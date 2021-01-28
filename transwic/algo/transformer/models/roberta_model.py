5d40a185dba6928518da9df01760de5d5ceb6053
HHansi <h.hansi1991@gmail.com>
2021-01-28 12:47:32 +0000

    009: Updated roberta model

--- transwic/algo/transformer/models/roberta_model.py
+++ transwic/algo/transformer/models/roberta_model.py
@@ -6,15 +6,19 @@ from torch.nn import CrossEntropyLoss, MSELoss
 from transformers import BertPreTrainedModel, ROBERTA_PRETRAINED_MODEL_ARCHIVE_LIST, RobertaConfig, RobertaModel
 from transformers.models.roberta.modeling_roberta import RobertaPreTrainedModel
 
+from transwic.algo.transformer.models.model_util import get_pooled_entity_output, get_first_entity_output, \
+    get_last_entity_output
+
 
 class RobertaClassificationHead(nn.Module):
     """Head for sentence-level classification tasks."""
 
-    def __init__(self, config):
+    def __init__(self, config, merge_n):
         super().__init__()
         self.dense = nn.Linear(config.hidden_size, config.hidden_size)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
+        # self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
+        self.out_proj = nn.Linear(config.hidden_size * merge_n, config.num_labels)
 
     def forward(self, features, **kwargs):
         # x = features[:, 0, :]  # take <s> token (equiv. to [CLS])
@@ -64,8 +68,15 @@ class RobertaForSequenceClassification(RobertaPreTrainedModel):
         self.num_labels = config.num_labels
 
         self.roberta = RobertaModel(config)
-        self.classifier = RobertaClassificationHead(config)
+
+        if merge_type is not None and "-pool" in merge_type:
+            self.pool = nn.AdaptiveAvgPool1d(config.hidden_size)
+
+        self.classifier = RobertaClassificationHead(config, merge_n)
         self.weight = weight
+
+        self.merge_type = merge_type
+        self.merge_n = merge_n
 
     def forward(
         self,
@@ -85,9 +96,55 @@ class RobertaForSequenceClassification(RobertaPreTrainedModel):
             position_ids=position_ids,
             head_mask=head_mask,
         )
-        # sequence_output = outputs[0]
-        sequence_output = outputs[1]
-        logits = self.classifier(sequence_output)
+        # # sequence_output = outputs[0]
+        # sequence_output = outputs[1]
+
+        # if entity positions are given, get embeddings at the given positions
+        if entity_positions is not None:
+            indices = [i for i in range(0, entity_positions.shape[0])]
+            tensor_indices = torch.tensor(indices, dtype=torch.long)
+
+            if self.merge_type is not None:
+                if "concat" in self.merge_type:
+                    list_pooled_output = []
+                    for i in range(0, entity_positions.shape[1]):
+                        temp_pooled_output = outputs[0][tensor_indices, entity_positions[:, i], :]
+                        list_pooled_output.append(temp_pooled_output)
+                    pooled_output = torch.cat(list_pooled_output, 1)
+
+                elif "add" in self.merge_type or "avg" in self.merge_type:
+                    pooled_output = outputs[0][tensor_indices, entity_positions[:, 0], :]
+                    for i in range(1, entity_positions.shape[1]):
+                        temp_pooled_output = outputs[0][tensor_indices, entity_positions[:, i], :]
+                        pooled_output = pooled_output.add(temp_pooled_output)
+                    if "avg" in self.merge_type:
+                        pooled_output = torch.div(pooled_output, entity_positions.shape[1])
+
+                elif "entity-pool" in self.merge_type:
+                    if entity_positions.shape[1] % 2 != 0:
+                        raise ValueError("begin or end of the entity is missing!")
+                    pooled_output = get_pooled_entity_output(outputs[0], entity_positions, self.pool)
+
+                elif "entity-first" in self.merge_type:
+                    pooled_output = get_first_entity_output(outputs[0], entity_positions, tensor_indices)
+
+                elif "entity-last" in self.merge_type:
+                    pooled_output = get_last_entity_output(outputs[0], entity_positions, tensor_indices)
+
+                else:  # If merge type is unkown
+                    raise KeyError(f"Unknown merge type found - {self.merge_type}")
+
+                if "cls-" in self.merge_type:
+                    pooled_output = torch.cat((outputs[1], pooled_output), 1)
+
+            else:  # if no merge type defined
+                raise KeyError("No merge type defined.")
+
+        # if no entity positions are given, get the embedding of CLS token
+        else:
+            pooled_output = outputs[1]
+
+        logits = self.classifier(pooled_output)
 
         outputs = (logits,) + outputs[2:]
         if labels is not None:
