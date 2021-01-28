0e0f02f72d96e6b53c5dc3724687a5f4deebcaaa
HHansi <h.hansi1991@gmail.com>
2021-01-28 11:07:48 +0000

    009: Updated roberta model

--- transwic/algo/transformer/models/roberta_model.py
+++ transwic/algo/transformer/models/roberta_model.py
@@ -4,7 +4,27 @@ from torch.nn import CrossEntropyLoss, MSELoss
 
 
 from transformers import BertPreTrainedModel, ROBERTA_PRETRAINED_MODEL_ARCHIVE_LIST, RobertaConfig, RobertaModel
-from transformers.models.roberta.modeling_roberta import RobertaClassificationHead, RobertaPreTrainedModel
+from transformers.models.roberta.modeling_roberta import RobertaPreTrainedModel
+
+
+class RobertaClassificationHead(nn.Module):
+    """Head for sentence-level classification tasks."""
+
+    def __init__(self, config):
+        super().__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
+
+    def forward(self, features, **kwargs):
+        # x = features[:, 0, :]  # take <s> token (equiv. to [CLS])
+        x = features
+        x = self.dropout(x)
+        x = self.dense(x)
+        x = torch.tanh(x)
+        x = self.dropout(x)
+        x = self.out_proj(x)
+        return x
 
 
 # class RobertaForSequenceClassification(BertPreTrainedModel):
@@ -65,7 +85,8 @@ class RobertaForSequenceClassification(RobertaPreTrainedModel):
             position_ids=position_ids,
             head_mask=head_mask,
         )
-        sequence_output = outputs[0]
+        # sequence_output = outputs[0]
+        sequence_output = outputs[1]
         logits = self.classifier(sequence_output)
 
         outputs = (logits,) + outputs[2:]
