0b1c17b429f22ea68562868d80a4a60dd2628e2b
HHansi <h.hansi1991@gmail.com>
2021-05-13 15:36:33 +0100

    010: Updated cross-lingual experiments with test data

--- examples/crosslingual/other/transformer.py
+++ examples/crosslingual/other/transformer.py
@@ -4,7 +4,8 @@ import torch
 
 from examples.common.config_validator import validate_transformer_config
 from examples.common.label_converter import decode, encode
-from examples.common.reader import read_test_file, read_training_file
+from examples.common.print_stat import print_information
+from examples.common.reader import read_test_file, read_training_file, read_data
 from examples.crosslingual.other.transformer_config import DATA_DIRECTORY, TEMP_DIRECTORY, \
     transformer_config, MODEL_TYPE, MODEL_NAME
 from transwic.algo.transformer.monotranswic import MonoTransWiCModel
@@ -20,16 +21,24 @@ train = train.rename(columns={'sentence1': 'text_a', 'sentence2': 'text_b', 'tag
 train = train[['text_a', 'text_b', 'labels']]
 train['labels'] = encode(train["labels"])
 
-data_config = {"test.en-ar.data": "test.en-ar",
-                "test.en-fr.data": "test.en-fr",
-                "test.en-ru.data": "test.en-ru",
-                "test.en-zh.data": "test.en-zh",
-               }
+# data_config = {"test.en-ar.data": "test.en-ar",
+#                 "test.en-fr.data": "test.en-fr",
+#                 "test.en-ru.data": "test.en-ru",
+#                 "test.en-zh.data": "test.en-zh",
+#                }
+data_config = {
+    "en_ar": ["test.en-ar.data", "test.en-ar.gold", "test.en-ar"],
+    "en_fr": ["test.en-fr.data", "test.en-fr.gold", "test.en-fr"],
+    "en_ru": ["test.en-ru.data", "test.en-ru.gold", "test.en-ru"],
+    "en_zh": ["test.en-zh.data", "test.en-zh.gold", "test.en-zh"],
+}
+
 
 for key, value in data_config.items():
+    test = read_data(os.path.join(DATA_DIRECTORY, value[0]), os.path.join(DATA_DIRECTORY, value[1]),
+                     args=transformer_config, cross_Lingual=True)
 
-    test = read_test_file(os.path.join(DATA_DIRECTORY, key), args=transformer_config, cross_Lingual=True)
-    test = test.rename(columns={'sentence1': 'text_a', 'sentence2': 'text_b'}).dropna()
+    test = test.rename(columns={'sentence1': 'text_a', 'sentence2': 'text_b', 'tag': 'labels'}).dropna()
     test_sentence_pairs = list(map(list, zip(test['text_a'].to_list(), test['text_b'].to_list())))
 
     # validate  configs
@@ -43,5 +52,11 @@ for key, value in data_config.items():
     test_predictions, test_raw_outputs = model.predict(test_sentence_pairs)
     test['predictions'] = test_predictions
     test['tag'] = decode(test['predictions'])
+
+    print(f'\n Evaluating {key}')
+    test['labels'] = decode(test['labels'])
+    print_information(test, "tag", "labels", "pos")
+
     test = test[['id', 'tag']]
-    test.to_json(os.path.join(TEMP_DIRECTORY, value), orient='records')
+    test.to_json(os.path.join(TEMP_DIRECTORY, value[2]), orient='records')
+
