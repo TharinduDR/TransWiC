e819705007522fdfab4268ea4718d4ab1cd476e9
HHansi <h.hansi1991@gmail.com>
2021-05-16 17:56:12 +0100

    009: Updated configs

--- examples/monolingual/other/transformer_config.py
+++ examples/monolingual/other/transformer_config.py
@@ -41,7 +41,7 @@ transformer_config = {
     "no_save": False,
     "save_recent_only": True,
     'save_model_every_epoch': False,
-    'n_fold': 3,
+    'n_fold': 1,
     'evaluate_during_training': True,
     "evaluate_during_training_silent": False,
     'evaluate_during_training_steps': 100,
